96004ed6c16f0341b3ff5d30b572014d2e420ed4
amsanjayx <sanjay.sharma6@student.amity.edu>
2022-08-01 17:45:50 +0530

    pronutrition

---
+++ pronutrition/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import FoodBox from './Data/FoodBox';
+
+
+function App() {
+  return (
+    <div className="App">
+      <FoodBox/>
+    </div>
+  );
+}
+
+export default App;
